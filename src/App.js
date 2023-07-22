f52172c1c517d63286bcc03bb8843da251b59abf
erkhabibullina <erkhabibullina@gmail.com>
2023-07-22 05:06:47 +0200

    fix app

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Home from './components/Home'
 import Welcome from './components/Welcome';
 import { Ipfs, ChartPoint } from './scripts/ipfs'
 import fs from "fs";
-import * as ethers from 'hardhat'
 
 function App() {
   return (
@@ -12,18 +11,5 @@ function App() {
     </div>
   );
 }
-
-(async () => {
-    const ipfsContact = ethers.getContractAt("IpfsStorage", "0x5FbDB2315678afecb367f032d93F642f64180aa3")
-    const ipfs = new Ipfs(ipfsContact);
-    await ipfs.addChartPoint(
-        new ChartPoint(
-            Date.now(),
-            10,
-            [5, 3, 2]
-        ),
-        "0x5FbDB2315678afecb367f032d93F642f64180aa3"
-    )
-})();
 
 export default App;
