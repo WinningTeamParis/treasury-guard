a2a4d18e509f31731687c3114a9812d0bfd849d1
Vlad Shlapakov <sshlapakovvlad@googlemail.com>
2023-07-23 04:04:52 +0200

    init hooks and plugins connection

--- account-creation/create-account.ts
+++ account-creation/create-account.ts
@@ -1,22 +1,25 @@
-import { ethers } from 'ethers'
-import { EthersAdapter } from '@safe-global/protocol-kit'
-import { SafeFactory } from '@safe-global/protocol-kit'
-import { SafeAccountConfig } from '@safe-global/protocol-kit'
+import {ethers} from 'ethers'
+import {EthersAdapter} from '@safe-global/protocol-kit'
+import {SafeFactory} from '@safe-global/protocol-kit'
+import {SafeAccountConfig} from '@safe-global/protocol-kit'
 import Safe from '@safe-global/protocol-kit'
-import { SafeTransactionDataPartial, OperationType } from '@safe-global/safe-core-sdk-types'
+import {SafeTransactionDataPartial, OperationType, SafeTransaction} from '@safe-global/safe-core-sdk-types'
 import protocolDeployments from "@safe-global/safe-core-protocol"
 
 import SafeApiKit from '@safe-global/api-kit'
 
 // https://chainlist.org/?search=goerli&testnets=true
-const RPC_URL='https://eth-goerli.public.blastapi.io'
+const RPC_URL = 'https://eth-goerli.public.blastapi.io'
+const MANAGER_ADDRESS = "0xAbd9769A78Ee63632A4fb603D85F63b8D3596DF9"
+const HOOKS_DATA = "0xfb63daa10000000000000000000000008599267fe1c8508f5c3b51858c89f09ac1658f87" // will change in the future
+const PLUGIN_DATA = "0x250db3c0000000000000000000000000719954b1689bd0afdec6e07a6e605d60938f79d30000000000000000000000000000000000000000000000000000000000000000" // will change in the future
 
 class LoggingProvider extends ethers.providers.JsonRpcProvider {
-  async send(method: string, params: any): Promise<any> {
-      console.log('Method:', method);
-      console.log('Params:', params);
-      return super.send(method, params);
-  }
+    async send(method: string, params: any): Promise<any> {
+        console.log('Method:', method);
+        console.log('Params:', params);
+        return super.send(method, params);
+    }
 }
 
 const provider = new LoggingProvider(RPC_URL);
@@ -25,8 +28,8 @@ const provider = new LoggingProvider(RPC_URL);
 const owner1Signer = new ethers.Wallet(process.env.PRIVATE_KEY!, provider)
 
 const ethAdapterOwner1 = new EthersAdapter({
-  ethers,
-  signerOrProvider: owner1Signer
+    ethers,
+    signerOrProvider: owner1Signer
 })
 
 const safeApiKit = new SafeApiKit({
@@ -34,32 +37,42 @@ const safeApiKit = new SafeApiKit({
     ethAdapter: ethAdapterOwner1
 })
 
-async function create_account() {
-    const safeFactory = await SafeFactory.create({ ethAdapter: ethAdapterOwner1 });
+async function create_safe() {
+    const safeAddress = await create_account()
+    await deposit(safeAddress)
+    const safeSdk = await Safe.create({ethAdapter: ethAdapterOwner1, safeAddress: safeAddress})
+    await enable_module(safeAddress, safeSdk)
+    await enable_hooks_or_plugins(safeAddress, safeSdk, HOOKS_DATA)
+    await enable_hooks_or_plugins(safeAddress, safeSdk, PLUGIN_DATA)
+}
+
+async function create_account(): Promise<any> {
+    const safeFactory = await SafeFactory.create({ethAdapter: ethAdapterOwner1});
 
     const safeAccountConfig: SafeAccountConfig = {
         owners: [
-          await owner1Signer.getAddress()
+            await owner1Signer.getAddress()
         ],
         threshold: 1,
         // ... (Optional params)
-      }
-    const safeSdkOwner1 = await safeFactory.deploySafe({ safeAccountConfig })
+    }
+    const safeSdkOwner1 = await safeFactory.deploySafe({safeAccountConfig})
 
     const safeAddress = await safeSdkOwner1.getAddress()
-      
+
     console.log('Your Safe has been deployed:')
     console.log(`https://goerli.etherscan.io/address/${safeAddress}`)
     console.log(`https://app.safe.global/gor:${safeAddress}`)
+    return safeAddress
 }
 
-async function deposit() {
+async function deposit(address: string) {
 
     const safeAmount = ethers.utils.parseUnits('0.05', 'ether').toHexString()
 
     const transactionParameters = {
-        to: "0x19D3fF6711b60eB1a4AA4126D7d3d305b72C465f",
-        value: safeAmount   
+        to: address,
+        value: safeAmount
     }
 
     const tx = await owner1Signer.sendTransaction(transactionParameters)
@@ -68,45 +81,13 @@ async function deposit() {
     console.log(`Deposit Transaction: https://goerli.etherscan.io/tx/${tx.hash}`)
 }
 
-async function send_transaction() {
-    // Create Safe instance
-    const safe = await Safe.create({
-        ethAdapter: ethAdapterOwner1,
-        safeAddress: "0x19D3fF6711b60eB1a4AA4126D7d3d305b72C465f"
-    })
-  
-  // Create transaction
-  const safeTransactionData: SafeTransactionDataPartial = {
-    to: '0x25238221BE3C80b7dDCD22CCB2Ff32cff32ecF91',
-    value: '1', // 1 wei
-    data: '0x',
-    operation: OperationType.Call
-  }
-  
-  const safeTransaction = await safe.createTransaction({ safeTransactionData })
-  
-  const senderAddress = await owner1Signer.getAddress()
-  const safeTxHash = await safe.getTransactionHash(safeTransaction)
-  const signature = await safe.signTransactionHash(safeTxHash)
-  
-  // Propose transaction to the service
-  await safeApiKit.proposeTransaction({
-    safeAddress: await safe.getAddress(),
-    safeTransactionData: safeTransaction.data,
-    safeTxHash,
-    senderAddress,
-    senderSignature: signature.data
-  })
-}
-
-async function enable_module() {
-    const safeSdk = await Safe.create({ ethAdapter: ethAdapterOwner1, safeAddress: "0x19D3fF6711b60eB1a4AA4126D7d3d305b72C465f" })
-    const safeTransaction = await safeSdk.createEnableModuleTx("0x4026BA244d773F17FFA2d3173dAFe3fdF94216b9");
+async function enable_module(address: string, safeSdk: Safe) {
+    const safeTransaction = await safeSdk.createEnableModuleTx(MANAGER_ADDRESS);
     const safeTxHash = await safeSdk.getTransactionHash(safeTransaction);
     const senderSignature = await safeSdk.signTransactionHash(safeTxHash)
 
     await safeApiKit.proposeTransaction({
-        safeAddress: "0x19D3fF6711b60eB1a4AA4126D7d3d305b72C465f",
+        safeAddress: address,
         safeTransactionData: safeTransaction.data,
         safeTxHash,
         senderAddress: await owner1Signer.getAddress(),
@@ -123,87 +104,68 @@ async function enable_module() {
     }
 }
 
-async function send_test_transaction() {
-    const safeSdk = await Safe.create({ ethAdapter: ethAdapterOwner1, safeAddress: "0x19D3fF6711b60eB1a4AA4126D7d3d305b72C465f" })
-
-    // Any address can be used. In this example you will use vitalik.eth
-    const destination = '0x25238221BE3C80b7dDCD22CCB2Ff32cff32ecF91'
-    const amount = ethers.utils.parseUnits('0.001', 'ether').toString()
-    
+async function enable_hooks_or_plugins(address: string, safeSdk: Safe, encodedData: string) {
     const safeTransactionData: SafeTransactionDataPartial = {
-      to: destination,
-      data: '0x',
-      value: amount
+        to: MANAGER_ADDRESS,
+        data: encodedData,
+        value: "0",
     }
-    // Create a Safe transaction with the provided parameters
-    const safeTransaction = await safeSdk.createTransaction({ safeTransactionData })
-    const safeTxHash = await safeSdk.getTransactionHash(safeTransaction);
+    const safeTransaction: SafeTransaction = await safeSdk.createTransaction({ safeTransactionData })
+    const safeTxHash = await safeSdk.getTransactionHash(safeTransaction)
     const senderSignature = await safeSdk.signTransactionHash(safeTxHash)
 
     await safeApiKit.proposeTransaction({
-        safeAddress: "0x19D3fF6711b60eB1a4AA4126D7d3d305b72C465f",
-        safeTransactionData: safeTransaction.data,
-        safeTxHash,
-        senderAddress: await owner1Signer.getAddress(),
-        senderSignature: senderSignature.data,
-    })
-    await safeApiKit.confirmTransaction(safeTxHash, senderSignature.data);
-
+            safeAddress: address,
+            safeTransactionData: safeTransaction.data,
+            safeTxHash,
+            senderAddress: await owner1Signer.getAddress(),
+            senderSignature: senderSignature.data
+        }
+    )
+    await safeApiKit.confirmTransaction(safeTxHash, senderSignature.data)
     const executeTxResponse = await safeSdk.executeTransaction(safeTransaction)
     const receipt = await executeTxResponse.transactionResponse?.wait()
 
-    console.log('Transaction executed:')
+    console.log("Hooks or plugins enabled:")
     if (receipt) {
         console.log(`https://goerli.etherscan.io/tx/${receipt.transactionHash}`)
     }
 }
-// attach plugin to test protocol manager
-
-// plugin: 0x6fe0dd0604E8615E494C08291375c570ab062E07
-// protocol manager: 0x4026BA244d773F17FFA2d3173dAFe3fdF94216b9
-// registry: 0x9EFbBcAD12034BC310581B9837D545A951761F5A
 
 
 async function transaction_from_plugin() {
     const pluginAddress = "0x719954B1689BD0AfdeC6E07A6e605d60938f79D3";
     const SAMPLE_PLUGIN_ABI = [
-      "function executeFromPlugin(address manager, address safe, bytes calldata data) external"
+        "function executeFromPlugin(address manager, address safe, bytes calldata data) external"
     ]
 
-    const getTestPlugin = async() => {
-      return new ethers.Contract(
-          pluginAddress,
-          SAMPLE_PLUGIN_ABI,
-          owner1Signer
-      )
+    const getTestPlugin = async () => {
+        return new ethers.Contract(
+            pluginAddress,
+            SAMPLE_PLUGIN_ABI,
+            owner1Signer
+        )
     }
 
     const plugin = await getTestPlugin();
 
     const safeProtocolInfo = protocolDeployments[5][0].contracts.TestSafeProtocolManager;
-    
-    const registryInfo = protocolDeployments[5][0].contracts.TestSafeProtocolRegistryUnrestricted;
-
-    const registry = new ethers.Contract(
-      registryInfo.address,
-      registryInfo.abi
-    );
 
     const manager = new ethers.Contract(
-      safeProtocolInfo.address,
-      safeProtocolInfo.abi,
-      provider
+        safeProtocolInfo.address,
+        safeProtocolInfo.abi,
+        provider
     );
 
     const amount = ethers.utils.parseUnits('0.001', 'ether').toString()
     const safeTransactionData = ethers.utils.defaultAbiCoder.encode(
-      ['address', 'bytes', 'uint256'],
-      [ "0x25238221BE3C80b7dDCD22CCB2Ff32cff32ecF91", '0x', amount]
+        ['address', 'bytes', 'uint256'],
+        ["0x25238221BE3C80b7dDCD22CCB2Ff32cff32ecF91", '0x', amount]
     );
-    
+
     console.log(manager.address);
     console.log(safeTransactionData);
-    await plugin.executeFromPlugin(manager.address, "0x19D3fF6711b60eB1a4AA4126D7d3d305b72C465f", safeTransactionData);
+    await plugin.executeFromPlugin(manager.address, "0x2DB14E367FbB4EB6B733709756d12De9b0B39B59", safeTransactionData);
 }
 
-transaction_from_plugin();
+create_safe();
