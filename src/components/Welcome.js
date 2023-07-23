416ee0c09942d856a3fcd6e99cb84cec43ff8809
phil3k3 <philip.limbeck@gmail.com>
2023-07-23 02:36:49 +0200

    comit

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Logo from '../svg/LogoSVG'
 import { Link } from 'react-router-dom'
 import logoPNG from './Group3.png'
 
@@ -6,6 +7,23 @@ import twitter from './Logo_of_Twitter.png'
 import google from './Google.png'
 import metamask from './metamask.png'
 import walletcon from './WalletConnect.svg'
+
+function setWallet(accounts: any) {
+  console.log(accounts);
+}
+
+const updateWallet = async (accounts:any) => {     
+    setWallet({ accounts })                         
+}   
+
+const metamaskEnabled = typeof window.ethereum !== "undefined";
+
+const handleConnect = async () => {                
+    let accounts = await window.ethereum.request({   
+      method: "eth_requestAccounts",                 
+    })                                               
+    updateWallet(accounts)                           
+}  
 
 
 export default function Welcome() {
@@ -25,8 +43,8 @@ export default function Welcome() {
         <div style={{ alignItems:'center',  display:'flex', textAlign:'center', justifyContent:'center'}}>
           <div style={{textAlign:'left'}}>
             <h1 className='text-xs'>Email</h1>
-            <input style={{width:'100%'}} placeholder='Email Address' type="text" name="" id="" />
-            <Link to="/join">
+            <input style={{width:'100%'}} type="text" name="" id="" />
+            <Link to="/home">
               <button style={{width:'100%', color:'white', backgroundColor:'#607ADD', marginTop: 10, height:'50px'}}>Connect with email</button>
             </Link>
             <button style={{ backgroundColor:'#E6E8ED', padding:'11px 24px', 
@@ -41,14 +59,14 @@ export default function Welcome() {
               </button>
               
               <button style={{ backgroundColor:'#E6E8ED', padding:'11px 24px', 
-                display: 'flex',
-                gap: 6,
-                width: '340px',
-                justifyContent: 'center',
-                alignItems: 'center',
-                marginTop: 10, height:'50px'}}>
-                <img src={google} alt="" />
-                Connect with Google
+              display: 'flex',
+              gap: 6,
+              width: '340px',
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginTop: 10, height:'50px'}}>
+              <img src={google} alt="" />
+              Connect with Google
               </button>
           </div>
         </div>
@@ -58,13 +76,13 @@ export default function Welcome() {
             <h1 className='text-sm mb-10' style={{color:'#607ADD'}}>
               External Wallet
             </h1>
-            <button style={{ backgroundColor:'#E6E8ED', padding:'11px 24px', 
+            <button onClick={{handleConnect}} style={{ backgroundColor:'#E6E8ED', padding:'11px 24px', 
               display: 'flex',
               gap: 6,
               width: '340px',
               justifyContent: 'center',
               alignItems: 'center',
-              marginTop: 10, height:'50px'}}>
+              marginTop: 10, height:'50px'}} onClick={handleConnect}>
               <img src={metamask} alt="" />
               Connect with Metamask
               </button>            
